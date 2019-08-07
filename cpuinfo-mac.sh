cd65d05450bcd9f0c694a068db57da99cea185f2
Wei Wang <w_w19980721@foxmail.com>
2019-08-07 14:47:36 +0800

    Add files via upload

--- cpuinfo-mac.sh
+++ cpuinfo-mac.sh
@@ -1,5 +1,5 @@
 #
-# Show CPU Information For Mac OS.
+# Show CPU Information for Mac OS.
 # Created by WW on 2019.8.1.
 # Copyright © WW. All rights reserved.
 #	
@@ -12,26 +12,32 @@ NTC=`system_profiler SPHardwareDataType|grep "Cores"| cut -f2 -d:`
 
 NTLC=`sysctl hw.logicalcpu| cut -f2 -d:`
 
+BRAND=`sysctl machdep.cpu.brand_string | cut -f2 -d:`
+
 let "NCPC = NTC/NPC "
 
 let "NHTC = NTLC/NTC"
 
-echo "***  CPU INFORMATION  ***"
+echo "   ***  CPU INFORMATION  ***"
 
 echo =====================================================
 
-echo Number of Physical CPUs: $NPC
+echo "#  $BRAND"
 
-echo Number of Cores in Every Physical CPU: $NCPC
+echo "#"
 
-echo Number of Total Cores: $NTC \(* shownodes\)
+echo "#   Number of Physical CPUs: $NPC"
+
+echo "#   Number of Cores in Every Physical CPU: $NCPC"
+
+echo "#   Number of Total Cores: $NTC (* shownodes)"
 
 echo -----------------------------------------------------
 
-echo Number of Hyper-Threadings in Every Core: $NHTC
+echo "#   Number of Hyper-Threadings in Every Core: $NHTC"
 
-echo Number of Toal Logical CPUs: $NTLC
+echo "#   Number of Toal Logical CPUs: $NTLC"
 
 echo =====================================================
 
-echo "***"
+echo "   ***"
