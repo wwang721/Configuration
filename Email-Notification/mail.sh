920ad0210e66fd4205212c42ebc50f3c7823a4a5
Wei Wang <w_w19980721@foxmail.com>
2020-07-16 19:16:11 +0800

    Add files via upload

--- Email-Notification/mail.sh
+++ Email-Notification/mail.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
+# Get date in English and custom format.
 TIME=`env LC_ALL=en_US.en date +'%H:%M:%S, %A, %m/%d/%Y %Z'`
 
-TEXT="Hi Wei,\n\nThe job (PID: $1 $2) you submitted have been finished at $TIME. Please\
+# $1 and $2 are input args of the **.sh file.
+TEXT="Hi Wei,\n\nThe job (PID: $1 $2) you submitted has been finished at $TIME. Please\
  go back to check the results as soon as possible!\n\nFaithfully,\nYour Mac"
 
 # Mail subject
 SUB="Update!"
 
-ADDR0="wangwei"    #localhost
-ADDR1="1653563@tongji.edu.cn"
-ADDR2="wei.wang@jhu.edu"
-ADDR3="13155590878@163.com"
+# E-mail address list
+ADDR[0]="wangwei"    #localhost
+ADDR[1]="1653563@tongji.edu.cn"
+ADDR[2]="wei.wang@jhu.edu"
+ADDR[3]="13155590878@163.com"
+ADDR[4]="ww000721@gmail.com"
 
-echo $TEXT|mail -s $SUB $ADDR3
+# Send emails
+for i in {0..4}
+do
+echo $TEXT|mail -s $SUB ${ADDR[$i]}
+done
--- Email-Notification/run.sh
+++ Email-Notification/run.sh
@@ -3,4 +3,6 @@
 # The executable file.
 EXEC="./test"
 
+# $! := PID of the last job.
+# Args following **.sh file are represented as $1, $2 ... in the **.sh file.
 nohup $EXEC > output && ./mail.sh $[$!+1] $EXEC &
